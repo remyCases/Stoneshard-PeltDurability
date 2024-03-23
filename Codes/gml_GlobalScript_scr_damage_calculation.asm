0cbdedcfd3b948ac6c155bb789b36255df578bed
remyCases <remy.cases@gmail.com>
2024-03-23 09:15:04 +0100

    [Fix] correct stacks operation

--- Codes/gml_GlobalScript_scr_damage_calculation.asm
+++ Codes/gml_GlobalScript_scr_damage_calculation.asm
@@ -1,65 +1,78 @@
+pushloc.v local._slashing
+pushi.e -5
 pushi.e 0
 push.v [array]self.peltloss_modifier
-pushloc.v local._slashing
 mul.d.v
+pushloc.v local._piercing
+pushi.e -5
 pushi.e 1
 push.v [array]self.peltloss_modifier
-pushloc.v local._piercing
 mul.i.v
 add.v.v
+pushloc.v local._blunt
+pushi.e -5
 pushi.e 2
 push.v [array]self.peltloss_modifier
-pushloc.v local._blunt
 mul.d.v
 add.v.v
+pushloc.v local._rending
+pushi.e -5
 pushi.e 3
 push.v [array]self.peltloss_modifier
-pushloc.v local._rending
 mul.i.v
 add.v.v
+pushloc.v local._fire
+pushi.e -5
 pushi.e 4
 push.v [array]self.peltloss_modifier
-pushloc.v local._fire
 mul.i.v
 add.v.v
+pushloc.v local._poison
+pushi.e -5
 pushi.e 5
 push.v [array]self.peltloss_modifier
-pushloc.v local._poison
 mul.d.v
 add.v.v
+pushloc.v local._frost
+pushi.e -5
 pushi.e 6
 push.v [array]self.peltloss_modifier
-pushloc.v local._frost
 mul.d.v
 add.v.v
+pushloc.v local._caustic
+pushi.e -5
 pushi.e 7
 push.v [array]self.peltloss_modifier
-pushloc.v local._caustic
 mul.i.v
 add.v.v
+pushloc.v local._shock
+pushi.e -5
 pushi.e 8
 push.v [array]self.peltloss_modifier
-pushloc.v local._shock
 mul.d.v
 add.v.v
+pushloc.v local._arcane
+pushi.e -5
 pushi.e 9
 push.v [array]self.peltloss_modifier
-pushloc.v local._arcane
 mul.d.v
 add.v.v
+pushloc.v local._psionic
+pushi.e -5
 pushi.e 10
 push.v [array]self.peltloss_modifier
-pushloc.v local._psionic
 mul.i.v
 add.v.v
+pushloc.v local._unholy
+pushi.e -5
 pushi.e 11
 push.v [array]self.peltloss_modifier
-pushloc.v local._unholy
 mul.i.v
 add.v.v
+pushloc.v local._sacred
+pushi.e -5
 pushi.e 12
 push.v [array]self.peltloss_modifier
-pushloc.v local._sacred
 mul.i.v
 add.v.v
 push.v arg.argument0
